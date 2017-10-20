17cacd19e484248ded897cbe59cc594e9a7ed914
Angel Newton <xanewton@gmail.com>
2017-10-20 14:38:30 -0400

    Add an action to connect the Stop recording button to the ModelView

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -27,5 +27,9 @@ class ViewController: UIViewController {
         recordingLabel.text = "Recording in progress..."
     }
     
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button pressed")
+        recordingLabel.text = "Tap to record"
+    }
 }
 
