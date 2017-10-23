2d48d5c039916f3e6cdd98e11b80c698054222cb
Angel Newton <xanewton@gmail.com>
2017-10-23 19:13:05 -0400

    Add buttons to the ViewControler

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecording: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
